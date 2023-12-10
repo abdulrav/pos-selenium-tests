e1709644bba9320b64095087ab4fb38286e7bf6d
hp1\abdulravoof <abdulrav86@gmail.com>
2023-12-10 17:33:35 +0530

    updated file docker

--- src/main/java/com/pos/testcases/CreateNewVendor.java
+++ src/main/java/com/pos/testcases/CreateNewVendor.java
@@ -1,5 +1,7 @@
 package com.pos.testcases;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
